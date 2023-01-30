181f496817ee87ccd0313637d21bfbb42a95e3bc
gyeongmin_yu <37259132+gyeongmin1120@users.noreply.github.com>
2023-01-30 14:57:27 +0900

    Update Hello World.java

--- 백준/Bronze/2557. Hello World/Hello World.java
+++ 백준/Bronze/2557. Hello World/Hello World.java
@@ -1,22 +1,6 @@
 
-import java.util.Scanner;
-
-public class no_1000 {
+public class no_2557 {
     public static void main(String[] args) {
-        long a = 0;
-        long b = 0;
-
-        Scanner scanner = new Scanner(System.in);
-        //System.out.print("A : ");
-        a = scanner.nextLong();
-
-        //System.out.print("B : ");
-        b = scanner.nextLong();
-
-        sum(a, b);
-    }
-
-    public static void sum(long a, long b){
-        System.out.println(a + b);
+        System.out.println("Hello Worlds!");
     }
 }
