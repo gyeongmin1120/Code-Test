89011fa5327e99b6878294627d967cbc5a17ad41
gyeongmin_yu <37259132+gyeongmin1120@users.noreply.github.com>
2023-01-31 16:58:31 +0900

    [Bronze V] Title: 나머지, Time: 124 ms, Memory: 14268 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer tokenizer = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(tokenizer.nextToken());
+        int b = Integer.parseInt(tokenizer.nextToken());
+        int c = Integer.parseInt(tokenizer.nextToken());
+
+        System.out.println((a+b)%c);
+        System.out.println(((a%c)+(b%c))%c);
+        System.out.println((a*b)%c);
+        System.out.println(((a%c)*(b%c))%c);
+    }
+}
