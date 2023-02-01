557c2f14ecbead3a7a05c5cc71f50a34b37da0e8
gyeongmin_yu <37259132+gyeongmin1120@users.noreply.github.com>
2023-02-01 16:50:56 +0900

    [Bronze V] Title: 고양이, Time: 116 ms, Memory: 13976 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args){
+        StringBuffer sb = new StringBuffer();
+
+        sb.append("\\    /\\");
+        sb.append("\n");
+        sb.append(" )  ( ')");
+        sb.append("\n");
+        sb.append("(  /  )");
+        sb.append("\n");
+        sb.append(" \\(__)|");
+
+        System.out.println(sb);
+    }
+
+}
