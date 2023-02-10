437e6f93a885c209380590440040e0af58210519
gyeongmin_yu <37259132+gyeongmin1120@users.noreply.github.com>
2023-02-11 01:23:57 +0900

    [level 0] Title: 중복된 숫자 개수, Time: 2.21 ms, Memory: 74.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120583. 중복된 숫자 개수/중복된 숫자 개수.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] array, int n) {
+        int answer = 0;
+        
+        answer = (int)Arrays.stream(array).filter(i -> i == n).count();
+        
+        return answer;
+    }
+}
