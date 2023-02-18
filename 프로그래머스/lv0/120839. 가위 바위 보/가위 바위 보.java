47f109279d788c78b72970ed981165707b7026bd
gyeongmin_yu <37259132+gyeongmin1120@users.noreply.github.com>
2023-02-18 21:03:18 +0900

    [level 0] Title: 가위 바위 보, Time: 2.45 ms, Memory: 78.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String solution(String rsp) {
+        String answer = "";
+        
+        for(char ch : rsp.toCharArray()){
+            if(ch == '2') answer += '0';
+            else if(ch == '0') answer += '5';
+            else if(ch == '5') answer += '2';
+        }
+        return answer;
+    }
+}
