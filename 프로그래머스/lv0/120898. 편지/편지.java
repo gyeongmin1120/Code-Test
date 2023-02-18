ac8671bce2231d20f793bd792f14c21207eb6cd0
gyeongmin_yu <37259132+gyeongmin1120@users.noreply.github.com>
2023-02-18 20:59:12 +0900

    [level 0] Title: 편지, Time: 0.02 ms, Memory: 72.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120898. 편지/편지.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(String message) {
+        int answer = 0;
+        
+        answer = (int)message.length() * 2;
+        
+        return answer;
+    }
+}
