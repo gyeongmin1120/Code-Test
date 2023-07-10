cb7d8a315cc7f2100a9f04736dee94f0fc55eef6
gyeongmin_yu <37259132+gyeongmin1120@users.noreply.github.com>
2023-07-10 21:49:30 +0900

    [unrated] Title: 날짜 비교하기, Time: 1.05 ms, Memory: 72.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181838. 날짜 비교하기/날짜 비교하기.java
@@ -0,0 +1,10 @@
+import java.time.*;
+
+class Solution {
+    public int solution(int[] date1, int[] date2) {
+        LocalDate ld1 = LocalDate.of(date1[0], date1[1], date1[2]);
+        LocalDate ld2 = LocalDate.of(date2[0], date2[1], date2[2]);
+        
+        return ld1.isBefore(ld2) ? 1 : 0;
+    }
+}
