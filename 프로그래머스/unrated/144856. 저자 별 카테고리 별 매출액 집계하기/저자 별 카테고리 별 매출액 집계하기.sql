38992afbf6af892a3ac9397b8584d72596ea8f22
gyeongmin_yu <37259132+gyeongmin1120@users.noreply.github.com>
2023-07-31 10:52:32 +0900

    [unrated] Title: 저자 별 카테고리 별 매출액 집계하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/144856. 저자 별 카테고리 별 매출액 집계하기/저자 별 카테고리 별 매출액 집계하기.sql
@@ -0,0 +1,63 @@
+
+
+-- SELECT
+--     -- A.AUTHOR_ID,
+--     -- A.AUTHOR_NAME,
+--     B.BOOK_ID,
+--     B.CATEGORY,
+--     SUM(SALES) * B.PRICE AS TOTAL_SALES
+-- FROM
+--     BOOK B INNER JOIN BOOK_SALES S
+--     ON B.BOOK_ID = S.BOOK_ID
+--     -- INNER JOIN AUTHOR A
+--     -- ON B.AUTHOR_ID = A.AUTHOR_ID
+-- WHERE
+--     TO_CHAR(SALES_DATE, 'YYYY-MM-DD') >= '2022-01-01'
+--     AND TO_CHAR(SALES_DATE, 'YYYY-MM-DD') <= '2022-01-31'
+-- GROUP BY
+--     B.BOOK_ID,
+--     -- A.AUTHOR_ID,
+--     -- A.AUTHOR_NAME,
+--     B.CATEGORY,
+--     B.PRICE
+-- ORDER BY
+--     A.AUTHOR_ID,
+--     B.CATEGORY DESC
+
+
+SELECT
+    A.AUTHOR_ID,
+    AUTHOR_NAME,
+    CATEGORY,
+    SUM(TOTAL_SALES)
+FROM
+    (
+        SELECT
+            B.BOOK_ID,
+            B.AUTHOR_ID,
+            B.CATEGORY,
+            SUM(SALES) * B.PRICE AS TOTAL_SALES
+        FROM
+            BOOK B INNER JOIN BOOK_SALES S
+            ON B.BOOK_ID = S.BOOK_ID
+        WHERE
+            TO_CHAR(SALES_DATE, 'YYYY-MM-DD') >= '2022-01-01'
+            AND TO_CHAR(SALES_DATE, 'YYYY-MM-DD') <= '2022-01-31'
+        GROUP BY
+            B.BOOK_ID,
+            B.AUTHOR_ID,
+            B.CATEGORY,
+            B.PRICE
+    ) S INNER JOIN AUTHOR A
+    ON S.AUTHOR_ID = A.AUTHOR_ID
+GROUP BY
+    A.AUTHOR_ID,
+    AUTHOR_NAME,
+    CATEGORY
+ORDER BY
+    A.AUTHOR_ID,
+    CATEGORY DESC
+
+
+
+
