0ff2bb37922fda58aa805f7482d986e403f8afa9
gyeongmin_yu <37259132+gyeongmin1120@users.noreply.github.com>
2023-08-20 16:18:39 +0900

    [level 1] Title: 폰켓몬, Time: 2.99 ms, Memory: 78.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] nums) {
+        Map<Integer, Integer> map = new HashMap();
+        
+        for(int num : nums)
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        
+        return (nums.length / 2) < map.size() ? (nums.length / 2) : map.size();
+    }
+}
